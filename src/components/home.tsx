deeb4f68e512694f2fa9d510b7741a0416f13d2c
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2025-07-28 13:08:35 +0530

    useMemo

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,29 +1,58 @@
 import React from 'react';
-import { useState,useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 
 const Home = () => {
-const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
+    const [dark, setTheme] = useState(false);
 
-const increment = () =>{
-    setCount(prev => prev + 1);
-    console.log("re-rendered",count)
-}
+    const doubleNumber = multiple(count)
 
-useEffect(() => {
-    setCount(count)
-    console.log("re-rendered-useEffect",count)
-    return () => {
-        setCount(0)
-        console.log("memory clean")
+    const increment = () => {
+        setCount(prev => prev + 1);
+        console.log("re-rendered", count)
     }
 
 
-},[count])
+    // const changeTheme = 
 
-return <button onClick={increment}>Count: {count}</button>
 
-}
+    const changeTheme = useMemo(() => {
+        return {
+            color: dark ? 'white' : 'black',
+            backgroundColor: dark ? 'black' : 'white'
+        }
+    }, [dark])
+    useEffect(() => {
+        setCount(count)
+        console.log("re-rendered-useEffect", count)
+        // return () => {
+        //     setCount(0)
+        //     console.log("memory clean")
+        // }
+
+    }, [count])
+
+    useEffect(() => {
+        console.log("Theme")
+    }, [changeTheme])
+
+
+
+    function multiple(num) {
+        return num * 2;
+    }
+
+    return (
+        <div>
+            {/* <button onClick={increment}>Count: {count}</button> */}
+            <input type="number" onChange={(e) => setCount(e.target.value)}></input>
+            <button style={changeTheme} onClick={() => setTheme((prev) => !prev)}>Toggle button</button>
+            <div>{doubleNumber}</div>
+        </div>
+
+    )
+};
 
 export default Home;
 
