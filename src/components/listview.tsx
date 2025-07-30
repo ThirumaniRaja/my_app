9fcde610a0d1c97e3d912d7447d7ed03252ed721
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2025-07-30 21:19:25 +0530

    commit
    useReduce

--- src/components/listview.tsx
+++ src/components/listview.tsx
@@ -1,24 +1,54 @@
-import { useState , useEffect, useContext} from "react";
+import { useState, useEffect, useContext, useReducer } from "react";
 import theme from "./list.tsx"
 
 
-const Listview = ({getList}) => {
+const Listview = ({ getList }) => {
     const [items, setItems] = useState([]);
-    const [themes] = useContext(theme);
+    const themes = useContext(theme); 
 
-    console.log(themes)
+    const useReducerFn = (state, action) => {
+        console.log(action,state)
+        switch (action) {
+            case 'increment':
+                return { count:  state.count + 1 };
+            case 'decrement':
+                if(state.count > 0)
+                return { count: state.count - 1 };
+            default:
+                return state;
 
-    useEffect(()=>{
+        }
+    }
+    // const [count, setCount] = useState(1);
+    const [state, dispatch] = useReducer(useReducerFn, { count: 1 })
+
+
+
+    const increment = () => {
+        dispatch('increment')
+    }
+
+    const decrement = () => {
+        dispatch('decrement')
+    }
+
+
+    console.log(state)
+
+    useEffect(() => {
         console.log("useeffect-listview")
         setItems(getList)
-    },[getList])
+    }, [getList])
     return (
         <div>
-            {items.map((item,index)=>{
-               return <p key={index} >{item}</p>
+            <button onClick={increment} >+</button> <br />
+            {state.count} <br />
+            <button onClick={decrement}>-</button>
+            {items.map((item, index) => {
+                return <p key={index} >{item}</p>
             })}
         </div>
     )
 }
 
- export default Listview;
+export default Listview;
