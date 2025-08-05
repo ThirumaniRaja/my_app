fd5784b39ac930d558ba9d2d97bdaba3634741c0
Thirumani Raja <nadar.thirumaniraja@mahindra.com>
2025-08-05 18:08:55 +0530

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 // import axios  from 'axios';
 import AxiosIn from './components/axios.js';
 // import DraggableBox from './components/DraggableB.tsx';
 // import PlantConfigView from './components/Configurable.tsx';
-// import Home from './components/home.tsx';
+import Home from './components/home.tsx';
 import List from './components/list.tsx';
 import ButtonC from './components/Button.tsx';
 import HigherOrder from './components/HigherOrder.tsx';
+import {Link,BrowserRouter, Routes, Route} from 'react-router-dom'
+import PlantConfigView from './components/Configurable.tsx';
 
 const App = () => {
+const [isLogin, setLogIn] = useState(false);
 const HigherO = HigherOrder(ButtonC);
  async function  apiCall(){
   const response = await AxiosIn.get('?format=json')
@@ -18,6 +21,12 @@ const HigherO = HigherOrder(ButtonC);
 
   return (
     <div className="App">
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/plant" element={<PlantConfigView />}/>
+      </Routes>
+      {/* </BrowserRouter> */}
       {/* <DraggableBox></DraggableBox>
       <PlantConfigView></PlantConfigView> */}
       
@@ -36,6 +45,12 @@ const HigherO = HigherOrder(ButtonC);
 
       <HigherO label="hello" trackInfo={{name:"Thiru"}}/>
       <HigherO label="hello" trackInfo={{name:"Raja"}}/>
+
+      <ul>
+        <li></li>
+       {isLogin && ( <Link to="/plant">plant</Link> )}
+        <li></li>
+      </ul>
 
       
 
